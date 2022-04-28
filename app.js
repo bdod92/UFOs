018de219ba2e4c8e3878ad44e34321533dc52911
Brayden <braydendodson@gmail.com>
2022-04-27 21:13:19 -0700

    clicks are handle

--- app.js
+++ app.js
@@ -27,8 +27,8 @@ function buildTable(data) {
       // Append a row to the table body
         let row = tbody.append("tr");
   
-      // Loop through each field in the dataRow and add
-      // each value as a table cell (td)
+        // Loop through each field in the dataRow and add
+        // each value as a table cell (td)
         // We're already working with an array of objects, where each object is a UFO sighting. By starting our line of code with Object.values, we're telling JavaScript to reference one object from the array of UFO sightings. By adding (dataRow) as the argument, we are saying that we want the values to go into the dataRow. We've added forEach((val) to specify that we want one object per row.
         Object.values(dataRow).forEach((val) => {
         let cell = row.append("td");
@@ -36,4 +36,29 @@ function buildTable(data) {
         }
       );
     });
-  }
+}
+
+function handleClick() {
+    // Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+  
+     // Check to see if a date was entered and filter the
+    // data using that date.
+    if (date) {
+      // Apply `filter` to the table data to only keep the
+      // rows where the `datetime` value matches the filter value
+      filteredData = filteredData.filter(row => row.datetime === date);
+    }
+  
+    // Rebuild the table using the filtered data
+    // @NOTE: If no date was entered, then filteredData will
+    // just be the original tableData.
+    buildTable(filteredData);
+}
+  
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
